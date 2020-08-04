8fc47735355d6c69bfdb403f63886d9dfec09f2d
tdomy <y.tokudomi@tdomy.com>
2020-08-04 21:23:48 +0900

    Updates

---
+++ precache-manifest.ade44a18d68680740b9bada7eb1193fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba0361348d788818a29",
+    "url": "/card-counter/css/app.39cdaca4.css"
+  },
+  {
+    "revision": "66b615b59ca5e6303acbaa51c7b78ea2",
+    "url": "/card-counter/index.html"
+  },
+  {
+    "revision": "cba0361348d788818a29",
+    "url": "/card-counter/js/app.51e3d005.js"
+  },
+  {
+    "revision": "1ca5559c6785b04951f6",
+    "url": "/card-counter/js/chunk-vendors.a986cfee.js"
+  },
+  {
+    "revision": "532f3abd9371dbf9db94b169f3a043fa",
+    "url": "/card-counter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/card-counter/robots.txt"
+  }
+]);
